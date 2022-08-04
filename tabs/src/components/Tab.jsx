7785c14cca22b5f6debf9e8cd3029df0aac72b69
Nitin Dagar <nitindagar@microsoft.com>
2022-08-04 12:50:12 +0530

    corrected url

--- tabs/src/components/Tab.jsx
+++ tabs/src/components/Tab.jsx
@@ -160,8 +160,8 @@ class Tab extends React.Component {
     }
     let url = window.location.origin;
     console.log("url=" + url);
-    let reponse = await axios.post('http://localhost:3978/api/sendAgenda', publishData, config);
-    //let reponse = await axios.post(`${window.location.origin}/api/sendAgenda`, publishData, config);
+   let reponse = await axios.post('http://localhost:3978/api/sendAgenda', publishData, config);
+   //let reponse = await axios.post('https://helloworlddevb65bdfbot.azurewebsites.net/api/sendAgenda', publishData, config);
     return reponse;
   }
 }
